7ee433b7f2369eef3ac6ad826cb3ae6e36c766a1
Maksim Moiseikin <m.moiseikin@gmail.com>
2019-10-25 20:40:03 +0200

    Initial commit

---
+++ app/src/main/java/me/maxdev/currencyconverter/MainActivity.kt
@@ -0,0 +1,19 @@
+package me.maxdev.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.maxdev.currencyconverter.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/me/maxdev/currencyconverter/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.maxdev.currencyconverter.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
