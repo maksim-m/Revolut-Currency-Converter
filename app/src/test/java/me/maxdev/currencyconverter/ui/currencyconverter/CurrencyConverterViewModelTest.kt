6288f480bf83c48b0bffc758a5fc6e7e25828a5b
Maksim Moiseikin <m.moiseikin@gmail.com>
2019-11-18 04:23:06 +0100

    Fix ViewModel tests

--- app/src/test/java/me/maxdev/currencyconverter/ui/currencyconverter/CurrencyConverterViewModelTest.kt
+++ app/src/test/java/me/maxdev/currencyconverter/ui/currencyconverter/CurrencyConverterViewModelTest.kt
@@ -2,27 +2,25 @@ package me.maxdev.currencyconverter.ui.currencyconverter
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import kotlinx.coroutines.Dispatchers
+import io.reactivex.Observable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
 import me.maxdev.currencyconverter.api.RatesResponse
-import me.maxdev.currencyconverter.data.Result
 import me.maxdev.currencyconverter.rates.CurrencyRatesRepository
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 import java.util.*
+
 
 @ExperimentalCoroutinesApi
 class CurrencyConverterViewModelTest {
-
-    private val mainThreadSurrogate = Dispatchers.Unconfined
-
     private lateinit var mockRepository: CurrencyRatesRepository
 
     private lateinit var viewModel: CurrencyConverterViewModel
@@ -32,27 +30,18 @@ class CurrencyConverterViewModelTest {
 
     @Before
     fun setUp() {
-        Dispatchers.setMain(mainThreadSurrogate)
+        RxJavaPlugins.setIoSchedulerHandler { scheduler -> Schedulers.trampoline() }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> Schedulers.trampoline() }
 
         mockRepository = mock {
         }
         viewModel = CurrencyConverterViewModel(mockRepository)
     }
 
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
-
     @Test
-    fun `show empty list after initialization`() = runBlockingTest {
-        assertThat(viewModel.rates.value).isEmpty()
-    }
-
-    @Test
-    fun `load items from repository`() = runBlockingTest {
-        whenever(mockRepository.getCurrencyRates()).thenReturn(
-            Result.Success(
+    fun `load currencies from repository`() {
+        whenever(mockRepository.getCurrencyRates("EUR")).thenReturn(
+            Observable.just(
                 RatesResponse(
                     "EUR",
                     Date(),
@@ -61,8 +50,111 @@ class CurrencyConverterViewModelTest {
             )
         )
 
-        viewModel.loadRates()
+        viewModel.onResume()
 
-        assertThat(viewModel.rates.value).containsExactly(CurrencyRateItem("USD"))
+        assertThat(viewModel.rates.value).containsExactly(
+            CurrencyRateItem("EUR", 10.0, editable = true),
+            CurrencyRateItem("USD", 10.0 * 1.2345)
+        )
+        assertThat(viewModel.showError.value).isFalse()
     }
+
+    @Test
+    fun `change base currency`() {
+        whenever(mockRepository.getCurrencyRates("RUB")).thenReturn(
+            Observable.just(
+                RatesResponse(
+                    "RUB",
+                    Date(),
+                    hashMapOf("USD" to 1.2345)
+                )
+            )
+        )
+        whenever(mockRepository.getCurrencyRates("EUR")).thenReturn(
+            Observable.just(
+                RatesResponse(
+                    "EUR",
+                    Date(),
+                    hashMapOf("USD" to 1.2345)
+                )
+            )
+        )
+
+        viewModel.onResume()
+        viewModel.onItemClicked(CurrencyRateItem("RUB", 20.0))
+
+        assertThat(viewModel.rates.value).containsExactly(
+            CurrencyRateItem("RUB", 20.0, editable = true),
+            CurrencyRateItem("USD", 20.0 * 1.2345)
+        )
+        assertThat(viewModel.showError.value).isFalse()
+    }
+
+    @Test
+    fun `change base amount`() {
+        whenever(mockRepository.getCurrencyRates("RUB")).thenReturn(
+            Observable.just(
+                RatesResponse(
+                    "RUB",
+                    Date(),
+                    hashMapOf("USD" to 1.2345)
+                )
+            )
+        )
+        whenever(mockRepository.getCurrencyRates("EUR")).thenReturn(
+            Observable.just(
+                RatesResponse(
+                    "EUR",
+                    Date(),
+                    hashMapOf("USD" to 1.2345)
+                )
+            )
+        )
+
+        viewModel.onResume()
+        viewModel.onBaseAmountChanged("100")
+
+        assertThat(viewModel.rates.value).containsExactly(
+            CurrencyRateItem("EUR", 100.0, editable = true),
+            CurrencyRateItem("USD", 100.0 * 1.2345)
+        )
+        assertThat(viewModel.showError.value).isFalse()
+    }
+
+    @Test
+    fun `show error message when no currency rates loaded`() {
+        whenever(mockRepository.getCurrencyRates(any<String>())).thenReturn(
+            Observable.error(IOException("Ooops!"))
+        )
+
+        viewModel.onResume()
+
+        assertThat(viewModel.showError.value).isTrue()
+    }
+
+    @Test
+    fun `remove error message when currencies are loaded after first error`() {
+        whenever(mockRepository.getCurrencyRates(any<String>())).thenReturn(
+            Observable.error(IOException("Ooops!"))
+        )
+
+        viewModel.onResume()
+
+        assertThat(viewModel.showError.value).isTrue()
+
+        whenever(mockRepository.getCurrencyRates(any<String>())).thenReturn(
+            Observable.just(
+                RatesResponse(
+                    "RUB",
+                    Date(),
+                    hashMapOf("USD" to 1.2345)
+                )
+            )
+        )
+
+        viewModel.onResume()
+
+        assertThat(viewModel.showError.value).isFalse()
+    }
+
 }
